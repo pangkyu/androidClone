be360f0aa83167017dbd33b93488aabcbe7072b8
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-12-07 19:44:07 +0900

    1207

--- app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
+++ app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package kr.ac.hs.and2021.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xffffffff));
     }
 }
